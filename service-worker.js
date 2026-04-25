45f1ad691e05344d1e941d7640dc13a745982084
david20040916 <david20040916@gmail.com>
2026-04-26 01:11:05 +0800

    Implement service worker for caching files

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = "snake-game-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "icon.png"
+];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
