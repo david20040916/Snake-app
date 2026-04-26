2bab17ccc8ff30e85b9348fdf400bc83f7a367af
david20040916 <david20040916@gmail.com>
2026-04-26 12:31:47 +0800

    Rename cache and update cached assets

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,15 @@
-const CACHE_NAME="snake-meme-god-v1";
+const CACHE="snake-max-v1";
 
 self.addEventListener("install",e=>{
 e.waitUntil(
-caches.open(CACHE_NAME).then(c=>c.addAll([
+caches.open(CACHE).then(c=>c.addAll([
 "./",
 "index.html",
 "manifest.json",
-"icon.png",
 "snake.png",
-"food.png"
+"food.png",
+"icon.png"
 ]))
-);
-});
-
-self.addEventListener("activate",e=>{
-e.waitUntil(
-caches.keys().then(keys=>{
-return Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));
-})
 );
 });
 
